15515158d732fa9150b1dbc88fb7f57c73a4af98
xerik <thecoffeejunkie@hotmail.de>
2015-06-15 18:05:44 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Stars and Stripes Theme
+
+Screenshots taken with font
+
+* Font "Anonymous Pro"
+* Font Size 14
+* Line Height 1.2
+
+![A screenshot of your theme](https://f.cloud.github.com/assets/69169/2289498/4c3cb0ec-a009-11e3-8dbd-077ee11741e5.gif)
---
+++ test.js
@@ -0,0 +1,53 @@
+
+console.log('console and debugger statements are red.');
+
+var a = new Array();
+var b = new Date();
+var c = "A string is green.";
+var d = [1,2,3,4];
+var e = undefined;
+var f = null;
+
+var hidden = function YouCannotSeeMe() {
+    //
+}
+
+/**
+ * A block comment is green.
+ *
+ * Functions have a grey background, allowing to easily
+ * spot anonymous functions.
+ *
+ */
+var myFunction = function(param1, param2) {
+    if(param1 === undefined || param2 === undefined) {
+        throw 'Throw is a keyword.'
+    }
+    return [param1, param2].join('');
+}
+
+jQuery.fn = jQuery.prototype = {
+	jquery: version,
+	constructor: jQuery,
+	length: 0,
+
+	toArray: function() {
+		return slice.call( this );
+
+        function() {} // every function has a grey background
+	},
+
+	get: function( num ) {
+        debugger;
+        // this is written in italics
+		return num != null ?
+			( num < 0 ? this[ num + this.length ] : this[ num ] ) :
+			slice.call( this );
+	}
+}
+
+String.prototype.doSomething = function () {
+    // A line comment is grey.
+};
+
+module.exports = myFunction;
